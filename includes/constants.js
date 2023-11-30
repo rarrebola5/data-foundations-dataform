152771955b16d063e949acd1c9fde8e5526b8113
Rafael Arrebola da Silva <144684124+rarrebola5@users.noreply.github.com>
2023-11-30 10:11:38 +0100

    Update constants.js

--- includes/constants.js
+++ includes/constants.js
@@ -1,4 +1,4 @@
-const pj_orch = dataform.projectConfig.vars.base_pj_ord + '-' + dataform.projectConfig.vars.executionSetting;
+const pj_orch = dataform.projectConfig.vars.base_pj_orch + '-' + dataform.projectConfig.vars.executionSetting;
 const pj_lz   = dataform.projectConfig.vars.base_pj_lz  + '-' + dataform.projectConfig.vars.executionSetting;
 const pj_raw  = dataform.projectConfig.vars.base_pj_raw + '-' + dataform.projectConfig.vars.executionSetting;
 const pj_dwh  = dataform.projectConfig.vars.base_pj_dwh + '-' + dataform.projectConfig.vars.executionSetting;
